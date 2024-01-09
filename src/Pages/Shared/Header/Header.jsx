66db3ac610c3ad70fdd87eae9040ac57bd78a9a9
Arif Hossan <arif.hossan.eu@gmail.com>
2024-01-09 22:49:11 +0600

    using moment in the header section

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+           <div className='text-center'>
+            <img src={logo} alt="taja_news_logo" />
+            <p className='text-secondary'>Travel Without Fear and Courage</p>
+            <h6 style={{color:"#3f557a"}}>{moment().format("dddd, MMMM, YYYY, h:mm a ")}</h6>
+           </div>
         </div>
     );
 };
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import Header from '../Pages/Shared/Header/Header';
 import Footer from '../Pages/Shared/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import RightNav from '../Pages/Shared/RightNav/RightNav';
+import LeftNav from '../Pages/Shared/LeftNav/LeftNav';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            <Container>
+                <Row>
+                    <Col lg={3}><LeftNav></LeftNav></Col>
+                    <Col lg={6}><h2>Main Component Coming Soon</h2></Col>
+                    <Col lg={3}><RightNav></RightNav></Col>
+                </Row>
+            </Container>
             <Footer></Footer>
         </div>
     );
