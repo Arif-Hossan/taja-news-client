624a6cc7ce60884e928551ed19394b0a9cc7e670
Arif Hossan <arif.hossan.eu@gmail.com>
2024-01-22 00:10:15 +0600

    presenting data to the news card

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
+import NewsCard from '../NewsCard/NewsCard';
 
 const Category = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const newsData = useLoaderData();
     return (
         <div>
-            <h2>Category news of {id}</h2>
+            <h2>Category news of {newsData.length}</h2>
+            {
+                newsData.map(news => <NewsCard key={news._id}
+                    news={news}></NewsCard>)
+            }
         </div>
     );
 };
